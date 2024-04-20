c14417164eb08b5c87aacab8227d1d393381d2be
Kamil Kasperek <72512501+Gorn456@users.noreply.github.com>
2024-04-20 20:45:37 +0000

    Third copilot commit

--- comments.js
+++ comments.js
@@ -1,67 +1,40 @@
-// create web server
-// create web server
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
+// Create web server and listen to port 3000
+// to run: $ node comments.js
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+var http = require('http');
+var fs = require('fs');
+var qs = require('querystring');
 
-  if (url === '/comments' && method === 'POST') {
-    const body = [];
-    req.on('data', chunk => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const comment = parsedBody.split('=')[1];
-      fs.appendFile('comments.txt', comment + '\n', (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  } else {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('<form action="/comments" method="POST"><input type="text" name="comment"><button type="submit">Add Comment</button></form>');
-    res.write('</html>');
-    return res.end();
-  }
+var server = http.createServer(function(req, res) {
+    if (req.url == '/comments.html') {
+        fs.readFile('comments.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
+    }
+    else if (req.url == '/comment') {
+        var postData = '';
+        req.on('data', function(chunk) {
+            postData += chunk;
+        });
+        req.on('end', function() {
+            postData = qs.parse(postData);
+            fs.appendFile('comments.txt', postData['comment'] + '\n', function(err) {
+                if (err) {
+                    console.log('Error writing file');
+                }
+            });
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<a href="/comments.html">Back</a>');
+            res.end();
+        });
+    }
+    else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('Page not found');
+        res.end();
+    }
 });
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-});
-// Path: comments.txt
-// this is a comment
-// this is another comment
-// this is a third comment
-// this is a fourth comment
-// this is a fifth comment
-// this is a sixth comment
-// this is a seventh comment
-// this is an eighth comment
-// this is a ninth comment
-// this is a tenth comment
-// this is an eleventh comment
-// this is a twelfth comment
-// this is a thirteenth comment
-// this is a fourteenth comment
-// this is a fifteenth comment
-// this is a sixteenth comment
-// this is a seventeenth comment
-// this is an eighteenth comment
-// this is a nineteenth comment
-// this is a twentieth comment
-// this is a twenty-first comment
-// this is a twenty-second comment
-// this is a twenty-third comment
-// this is a twenty-fourth comment
-// this is a twenty-fifth comment
-// this is a twenty-sixth comment
-// this is a twenty-seventh comment
-//
+console.log('Server running at http://') ;
